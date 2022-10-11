16229d54ff770951a87c9191fee74b6eb30ffe4c
Sara894 <71623298+Sara894@users.noreply.github.com>
2022-10-11 07:35:54 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+bool is_prime(int x) {
+  for (int d = 2; d * d < x; d++) {
+    if (x % d == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+vector<int> raz(int n) {
+  vector<int> arr = {};
+  for (int i = 2; i<=n && n > 1 ;)
+    if (n % i == 0 && is_prime(i)) {
+        arr.push_back(i);
+        n /= i;
+    }
+  else if(n % i != 0){
+    i++;
+  }
+  return arr;
+}
+
+int Lezhandr(int a, int p){
+  if(a % p == 0 )
+  {
+     cout<<"Число "<<a<<" является квадратичным вычетом по модулю "<<p<<".\n";
+    cout<<"По определению символ Лежандра "<< a <<"/"<< p<<" равен 0.\n";
+    return 0;
+  }
+  else if(is_prime(p)){
+    cout<<"Применим критерий Эйлера: \n";
+    cout<<"Найдем значение символа Лежандра "<<a <<"/"<<p<<".\n";
+    cout<<a<<"^("<<p<<"-1)/2 = "<<a <<"/"<<p<<" mod p \n";
+    long long int temp = pow(a,(p-1)/2);
+    cout<<a<<"^("<<p<<"-1)/2 = "<<temp<<".\n";
+    long long int mod = temp % p;
+    cout<<temp<<" по модулю "<<p<<" = "<<mod<<"\n";
+    cout<<"По критерию Эйлера символ Лежандра равен "<<mod<<".\n";
+    return mod;
+ }
+}
+
+
+
+int main() {
+  cout<<"Сколько линейных сравнений вы хотите проверить: ";
+  int l;
+  cin>>l;
+ while(l--){
+    std::cout << "\nимеет ли решение сравнение  x^2 = a mod p.\n";
+  std::cout<<"Введите числа a, p через пробел: ";
+  int a, p;
+  cin>>a>>p;
+  if(a % p == 0 )
+  {
+     cout<<"Число а является квадратичным вычетом по модулю "<<p<<".\n";
+    cout<<"По определению символ Лежандра "<< a <<"/"<< p<<" равен 0.\n";
+  }
+  else if(is_prime(p)){
+    cout<<"Применим критерий Эйлера: \n";
+    cout<<"Найдем значение символа Лежандра "<<a <<"/"<<p<<".\n";
+    cout<<a<<"^("<<p<<"-1)/2 = "<<a <<"/"<<p<<" mod p \n";
+    int temp = pow(a,(p-1)/2);
+    cout<<a<<"^("<<p<<"-1)/2 = "<<temp<<".\n";
+    int mod = temp % p;
+    cout<<temp<<" по модулю "<<p<<" = "<<mod<<"\n";
+    cout<<"По критерию Эйлера символ Лежандра равен "<<mod<<".\n";
+    if(mod == 1)
+      cout<<"Cледовательно, данное квадратичное сравнение имеет решение.\n";
+    else if(mod == -1)
+      cout<<"Значит, данное квадратичное сравнение решений не имеет.\n";
+ }
+   else if(!is_prime(p)){
+     cout<<"Найдем значение символа Якоби "<<a <<"/"<<p<<".\n";
+     vector<int> v = raz(p);
+     cout<<"По определению символа Якоби "<<a <<"/"<<p<<" = ";
+     vector<int> lezhandrs; 
+     for(int i = 0; i <= v.size(); i++){
+       cout<<a<<"/"<<v[i]<<" ";
+       int tem = Lezhandr(a,v[i]);
+       lezhandrs.push_back(tem);
+       if(i != v.size()-1)
+         cout<<"* ";
+     }
+
+     int res = 1;
+     cout<<"Найдем значение каждого из сомножителей(являющихся символами Лежандра) и вычислим так символ Якоби:\n";
+     for(int i = 0; i < lezhandrs.size();i++){
+       res*=lezhandrs[i];
+     }
+
+      cout<<"По символ Якоби равен "<<res<<".\n";
+    if(res == 1)
+      cout<<"Cледовательно, данное квадратичное сравнение имеет решение.\n";
+    else if(res == -1)
+      cout<<"Значит, данное квадратичное сравнение решений не имеет.\n";
+     
+    
+   }
+  }
+
+
+
+  
+}
